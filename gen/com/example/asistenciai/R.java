fa4c4517717efd743309f01dbb641252a6d56265
JPierre Perez Tang <yampier123@hotmail.com>
2013-05-29 00:34:45 -0500

    Mejoras

--- gen/com/example/asistenciai/R.java
+++ gen/com/example/asistenciai/R.java
@@ -52,10 +52,12 @@ public final class R {
         public static final int key=0x7f020001;
         public static final int launcher_icon=0x7f020002;
         public static final int logo=0x7f020003;
-        public static final int qr=0x7f020004;
-        public static final int share_via_barcode=0x7f020005;
-        public static final int shopper_icon=0x7f020006;
-        public static final int top=0x7f020007;
+        public static final int logo_usmp=0x7f020004;
+        public static final int qr=0x7f020005;
+        public static final int registro=0x7f020006;
+        public static final int share_via_barcode=0x7f020007;
+        public static final int shopper_icon=0x7f020008;
+        public static final int top=0x7f020009;
     }
     public static final class id {
         public static final int BtnIngresar=0x7f080010;
