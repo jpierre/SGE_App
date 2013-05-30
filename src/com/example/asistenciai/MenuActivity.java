1f741bd1a4ce63b6dc5b7f563b4f8e117285e688
JPierre Perez Tang <yampier123@hotmail.com>
2013-05-30 01:09:59 -0500

    Falta poco..

--- src/com/example/asistenciai/MenuActivity.java
+++ src/com/example/asistenciai/MenuActivity.java
@@ -1,14 +1,32 @@
 package com.example.asistenciai;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+
+
 public class MenuActivity extends Activity {
+	
+	JSONParser jsonParser = new JSONParser();
+	// url to update product
+	private static final String url_update_registro = "http://10.0.2.2/android_connect/registrar_asistencia.php";
+	
+	private static final String TAG_SUCCESS = "success";
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +53,10 @@ public class MenuActivity extends Activity {
 	 	   if (requestCode == 0) {
 	 	      if (resultCode == RESULT_OK) {
 	 	         String contents = intent.getStringExtra("SCAN_RESULT");
-	 	        if(contents.equals("prueba")){
-	 	        	Intent i = new Intent(MenuActivity.this, ResultadoOkActivity.class);
-	 	        	startActivity(i);  
+	 	        if(contents.equals("30")){
+	 	        	new SaveProductDetails().execute();
+	 	        	/*Intent i = new Intent(MenuActivity.this, ResultadoOkActivity.class);
+	 	        	startActivity(i);*/  
 	 	         }else{ 
 	 	        	Intent i = new Intent(MenuActivity.this, ResultadoFalloActivity.class);
 	 	        	startActivity(i);
@@ -47,5 +66,49 @@ public class MenuActivity extends Activity {
 	 	      }
 	 	   }
 	 	}
-	
+		
+		class SaveProductDetails extends AsyncTask<String, String, String> {
+
+			/**
+			 * Saving product
+			 * */
+			protected String doInBackground(String... args) {
+
+				/*// getting updated data from EditTexts
+				String name = txtName.getText().toString();
+				String price = txtPrice.getText().toString();
+				String description = txtDesc.getText().toString();*/
+				String num_doc_user = "30";
+				// Building Parameters
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("num_doc_user", num_doc_user));
+				
+				// sending modified data through http request
+				// Notice that update product url accepts POST method
+				JSONObject json = jsonParser.makeHttpRequest(url_update_registro,
+						"POST", params);
+
+				// check json success tag
+				try {
+					int success = json.getInt(TAG_SUCCESS);
+					
+					if (success == 1) {
+						Intent i = new Intent(MenuActivity.this, ResultadoOkActivity.class);
+		 	        	startActivity(i);
+						/*// successfully updated
+						Intent i = getIntent();
+						// send result code 100 to notify about product update*/
+						setResult(100, i);
+						finish();
+					} else {
+						Intent i = new Intent(MenuActivity.this, ResultadoFalloActivity.class);
+		 	        	startActivity(i);
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+
+				return null;
+			}
+		}
 }
